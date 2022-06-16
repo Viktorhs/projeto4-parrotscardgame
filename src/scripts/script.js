cb2d41435e320e5e62bfcd9b0886ee19c8f9ff60
Viktorhs <victor.hugo.gba@hotmail.com.br>
2022-06-16 18:17:39 -0300

    Adicionado a função para permiter apenas numeros pares dentro do intervalo de 4 a 14

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,13 +1,21 @@
 function inciarJogo() {
-    let qtd = prompt("quantas cartas deseja?");
+    let qtd = Number(prompt("Escolha o numero de cartas entre 4 a 14"));
+    let proseguir = 0;
+    while(proseguir < 1) {
+        
+        if(qtd%2 === 0 && qtd > 3 && qtd <15){
+            proseguir ++;
+        }else{
+            qtd = Number(prompt("Escolha o numero de cartas entre 4 a 14"));}
+    }
+
     let carta = document.querySelector("main")
     carta.innerHTML = ""
 
     for(let i = 0; i < qtd; i++)
-        carta.innerHTML += `    <div class="carta" onclick="virarCarta(this)">
-                                    <img src="./src/images/front.png" alt="">
-                                </div>
-    `
+        carta.innerHTML += `<div class="carta" onclick="virarCarta(this)">
+                                <img src="./src/images/front.png" alt="">
+                            </div>`
 }
 
 function virarCarta(elemento) {
